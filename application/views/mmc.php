b156ef928eb479fb7a2ef2ff56582fa95b9d000b
Bram Bleys <b.bleys@hotmail.com>
2018-03-02 13:49:45 +0100

    CHanged index + added files

---
+++ application/views/mmc.php
@@ -0,0 +1,133 @@
+<!doctype html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <!-- Eigen CSS -->
+    <link rel="stylesheet" href="assets/css/mmc.css">
+
+    <title>Minder Mobielen Centrale</title>
+</head>
+<body>
+    <header>
+        <nav class="navbar navbar-expand-lg sticky-top">
+            <div class="container">
+                <a class="navbar-brand" href="#">MMC</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#primaryNav" aria-controls="primaryNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="primaryNav">
+                    <div class="navbar-nav">
+                        <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
+                        <a class="nav-item nav-link" href="#">Contact</a>
+                    </div>
+                </div>
+            </div>
+        </nav>
+        <div class="jumbotron jumbotron-fluid">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-2">
+                        <img src="assets/images/logo.png" alt="Logo Minder Mobielen Centrale" class="img-fluid">
+                    </div>
+                    <div class="col-lg-10">
+                        <h1 class="display-4">Minder Mobielen Centrale</h1>
+                    </div>
+                    <div class="col-12">
+                        <hr class="my-4">
+                        <p>
+                            Minder mobiele personen met een beperkt inkomen kunnen beroep doen op deze dienst waarbij chauffeurs hen vervoeren tegen de prijs van € 0,30 per km. De chauffeurs rijden op vrijwillige basis en dit met hun eigen wagen.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-8">
+                <main>
+                    <div class="row">
+                        <div class="col">
+                            <h2>Hoe werkt de MMC?</h2>
+                            <p>
+                                Leden reserveren telefonisch elke rit met de centrale minstens twee dagen op voorhand, alleen dan geldt de omniumverzekering. Alle bijzonderheden omtrent de rit wordt aan de centrale doorgegeven: het aantal personen die meerijden, het aantal haltes, .... De centrale neemt vervolgens contact op met de chauffeurs.
+                            </p>
+                        </div>
+                        <div class="col">
+                            <h2>Hoe word je lid?</h2>
+                            <p>
+                                Je bent pas lid als je een lidkaart van de MMC bezit. Deze kan je verkrijgen op de sociale dienst tegen te betaling van € 10 per jaar. Vanaf 1 juli betaal je slechts € 5 tot eind december. In dit bedrag zit de omniumverzekering begrepen. Als chauffeur kan je contact met ons opnemen of jezelf.
+                            </p>
+                        </div>
+                        <div class="col-12">
+                            <h2>Voorwaarden voor de gebruikers</h2>
+                            <ul>
+                                <li>Je inkomen is lager dan maximum 2X het leefloon.</li>
+                                <li>Je betaalt jaarlijks het lidgeld.</li>
+                                <li>Per gereden km betaal je € 0,30.</li>
+                                <li>De km’s beginnen te tellen van het thuisadres van de chauffeur en eindigen als de chauffeur terug thuis aangekomen is.</li>
+                                <li>Je rekent per rit af met de chauffeur.</li>
+                                <li>Afspraken betreffende al dan niet roken, eten, drinken in de wagen worden met de chauffeur gemaakt.</li>
+                            </ul>
+                        </div>
+                    </div>
+
+                </main>
+            </div>
+            <div class="col-lg-3 offset-lg-1">
+                <aside>
+                    <h2>Title - Side navigation</h2>
+                    <nav>
+                    </nav>
+                </aside>
+            </div>
+        </div>
+    </div>
+    <footer class="footer">
+        <div class="footer-main d-flex align-items-center">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-6">
+
+                    </div>
+                    <div class="col-lg-6 text-right">
+                        <h3>Contactgegevens</h3>
+                        <p>
+                            Tel:  070 22 22 92<br>
+                            E-mail: mmc@taxistop.be<br>
+                            Andere websites: www.taxistop.be
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="footer-copyright">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+                        <hr class="hr-footer">
+                    </div>
+                    <div class="col">
+                        <p class="text-left">Made with (l) by <a href="#">Brogrammers</a></p>
+                    </div>
+                    <div class="col">
+                        <p class="text-right">&copy; MMC - 2018</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</body>
+</html>
