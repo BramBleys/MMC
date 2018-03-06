be5b4e62895514e9d7bf19ae34f4702ad6bcbb74
Bram Bleys <b.bleys@hotmail.com>
2018-03-06 16:20:33 +0100

    gitignore test 3

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 $config['base_url'] = 'http://localhost:8090';
-//deze comment is een test 2
+//deze comment is een test 3
 
 /*
 |--------------------------------------------------------------------------
