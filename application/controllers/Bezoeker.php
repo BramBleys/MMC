03bbddb00ad023db643215e90112973d6fe20fee
Bram Bleys <b.bleys@hotmail.com>
2018-03-09 15:37:20 +0100

    Les 9 maart

--- application/controllers/Bezoeker.php
+++ application/controllers/Bezoeker.php
@@ -3,7 +3,16 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Bezoeker extends CI_Controller {
 
-    public function login() {
+    public function __construct() {
+        parent::__construct();
+    }
 
+    public function login() {
+        $data['titel'] = 'Inloggen';
+        $data['gebruiker'] = $this->authex->getGebruikerInfo();
+
+
+
+        $this->template->load('main_master', $partials, $data);
     }
 }
---
+++ application/libraries/Authex.php
@@ -0,0 +1,57 @@
+<?php
+
+if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+class Authex {
+
+    public function __construct() {
+        $CI = &get_instance();
+
+        $CI->load->model('bezoeker_model');
+    }
+
+    function getGebruikerInfo() {
+        // geef gebruiker-object als gebruiker aangemeld is
+        $CI = &get_instance();
+
+        if (!$this->isAangemeld()) {
+            return null;
+        } else {
+            $id = $CI->session->userdata('gebruiker_id');
+            return $CI->bezoeker_model->get($id);
+        }
+    }
+
+    function isAangemeld() {
+        // gebruiker is aangemeld als sessievariabele gebruiker_id bestaat
+        $CI = &get_instance();
+
+        if ($CI->session->has_userdata('gebruiker_id')) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    function meldAan($email, $wachtwoord) {
+        // gebruiker aanmelden met opgegeven email en wachtwoord
+        $CI = &get_instance();
+
+        $gebruiker = $CI->gebruiker_model->getGebruiker($email, $wachtwoord);
+
+        if ($gebruiker == null) {
+            return false;
+        } else {
+            $CI->gebruiker_model->updateLaatstAangemeld($gebruiker->id);
+            $CI->session->set_userdata('gebruiker_id', $gebruiker->id);
+            return true;
+        }
+    }
+
+    function meldAf() {
+        // afmelden, dus sessievariabele wegdoen
+        $CI = &get_instance();
+
+        $CI->session->unset_userdata('gebruiker_id');
+    }
+}
