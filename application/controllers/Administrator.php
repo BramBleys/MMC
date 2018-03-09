4651aa23a9c370813b42d9d3c5fe2dc04f0dff6c
FastenakelsK <r0624930@student.thomasmore.be>
2018-03-09 15:18:59 +0100

    Added helper form to autoload, changes to administrator controller and parameters view

--- application/controllers/Administrator.php
+++ application/controllers/Administrator.php
@@ -9,19 +9,19 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Administrator extends CI_Controller
 {
-    public function __construct()
-    {
+    public function __construct(){
         parent::__construct();
         $this->load->library('Template');
     }
 
-    public function index()
-    {
+    public function index(){
         $data['titel'] = 'Parameters';
 
-        $partials = array( 'navigatie' => 'main_gebruiker');
+        $partials = array( 'navigatie' => 'main_gebruiker', 'inhoud' => 'administrator/parameters');
         $this->template->load('main_master', $partials, $data);
+    }
 
-        //$this->load->view('administrator/parameters');
+    public function parametersOpslagen(){
+
     }
 }
