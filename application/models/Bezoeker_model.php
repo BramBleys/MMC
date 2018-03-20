ca1e2372d2aa34bcf88a3c3e8a9d0da572c62555
Bram Bleys <b.bleys@hotmail.com>
2018-03-20 18:42:22 +0100

    Moved gebruiker functions from bezoeker_model to gebruiker_model

--- application/models/Bezoeker_model.php
+++ application/models/Bezoeker_model.php
@@ -5,49 +5,5 @@ class Bezoeker_model extends CI_Model {
     function __construct() {
         parent::__construct();
     }
-
-    function get($id) {
-        // geef gebruiker-object met opgegeven id
-        $this->db->where('id', $id);
-        $query = $this->db->get('Gebruiker');
-        return $query->row();
-    }
-
-    function getGebruiker($gebruikersnaam, $wachtwoord) {
-        // geef gebruiker-object met gebruikersnaam en wachtwoord
-        $this->db->where('gebruikersnaam', $gebruikersnaam);
-        $query = $this->db->get('Gebruiker');
-
-        if ($query->num_rows() == 1) {
-            $gebruiker = $query->row();
-            // controleren of het wachtwoord overeenkomt
-            if ($wachtwoord == $gebruiker->wachtwoord) {
-                return $gebruiker;
-            } else {
-                return null;
-            }
-        } else {
-            return null;
-        }
-    }
-
-    function getGebruikerWithSoort($id) {
-        //geef gebruiker-object met soort door opgegeven id
-        $this->db->order_by('id', 'asc');
-        $query = $this->db->where('id', $id);
-        $gebruiker = $query->row();
-
-        $this->load->model('soort_model');
-        $gebruiker->soort = $this->soort_model->getSoort($gebruiker->id);
-
-        return $gebruiker;
-    }
-
-    /*function getTekst($paginaId) {
-        $this->db->order_by('id', 'asc');
-
-        $query = $this->db->where('id', $paginaId);
-        return $query->result();
-    }*/
 }
 
--- application/models/Gebruiker_model.php
+++ application/models/Gebruiker_model.php
@@ -1,9 +1,46 @@
 <?php
 
-    class gebruiker_model extends CI_Model {
+    class Gebruiker_model extends CI_Model {
         function update($gegevens) {
             //update de gegevens van de gebruiker waar je aanpassingen voor hebt gedaan
-            $this->db->where('id',$gegevens->id);
+            $this->db->where('id', $gegevens->id);
             $this->db->update('gebruiker', $gegevens);
+        }
+
+        function get($id) {
+            // geef gebruiker-object met opgegeven id
+            $this->db->where('id', $id);
+            $query = $this->db->get('Gebruiker');
+            return $query->row();
+        }
+
+        function getGebruiker($gebruikersnaam, $wachtwoord) {
+            // geef gebruiker-object met gebruikersnaam en wachtwoord
+            $this->db->where('gebruikersnaam', $gebruikersnaam);
+            $query = $this->db->get('Gebruiker');
+
+            if ($query->num_rows() == 1) {
+                $gebruiker = $query->row();
+                // controleren of het wachtwoord overeenkomt
+                if ($wachtwoord == $gebruiker->wachtwoord) {
+                    return $gebruiker;
+                } else {
+                    return null;
+                }
+            } else {
+                return null;
+            }
+        }
+
+        function getGebruikerWithSoort($id) {
+            //geef gebruiker-object met soort door opgegeven id
+            $this->db->order_by('id', 'asc');
+            $query = $this->db->where('id', $id);
+            $gebruiker = $query->row();
+
+            $this->load->model('soort_model');
+            $gebruiker->soort = $this->soort_model->getSoort($gebruiker->id);
+
+            return $gebruiker;
         }
     }
