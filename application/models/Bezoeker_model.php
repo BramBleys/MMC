98302475480fbecaa2fecfdc2102745d48c9c848
Bram Bleys <b.bleys@hotmail.com>
2018-03-23 14:26:20 +0100

    Changed bezoeker_model for Kilian

--- application/models/Bezoeker_model.php
+++ application/models/Bezoeker_model.php
@@ -9,7 +9,7 @@ class Bezoeker_model extends CI_Model {
     function getInhoud($paginaId){
         $this->db->order_by('naam', 'ASC');
         $this->db->where('paginaId', $paginaId);
-        $query = $this->db->get('inhoud');
+        $query = $this->db->get('Inhoud');
 
         return $query->result();
     }
