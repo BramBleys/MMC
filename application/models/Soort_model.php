eb33e15ee29edcc8754f558429881fd3f92d3097
Bram Bleys <b.bleys@hotmail.com>
2018-05-16 17:39:24 +0200

    Deleted pagina_model, changed Soort_model

--- application/models/Soort_model.php
+++ application/models/Soort_model.php
@@ -1,5 +1,10 @@
 <?php
-
+    /**
+     * @class Soort_model
+     * @brief Model-klasse voor de soorten gebruikers
+     *
+     * Model-klasse die alle methodes bevat om te interageren met de database-tabel soort
+     */
 class Soort_model extends CI_Model {
 
     /**
@@ -9,6 +14,12 @@ class Soort_model extends CI_Model {
         parent::__construct();
     }
 
+    /**
+     * Retourneert de soort van de gebruiker met gebruikerId = $gebruikerId
+     *
+     * @param $id de id van de gebruiker
+     * @return Een soort object
+     */
     function getSoort($id){
         //Geef de soort van de gebruiker afhankelijk van zijn id
         $this->db->order_by('id', 'asc');
